f7ad120ac61c1cb4078a6c350d1c0cebe121860a
Furkan Tas <tasfurkan1995@gmail.com>
2024-05-06 16:10:56 +0300

    No commment

--- scripts/app.js
+++ scripts/app.js
@@ -22,6 +22,8 @@ const updateUI = (data) => {
     `;
 
     // update the night/day & icon images
+    const iconSrc = `img/icons/${weather.WeatherIcon}.svg`;
+    icon.setAttribute('src', iconSrc);
 
     let timeSrc = null;
     if(weather.IsDayTime) {
